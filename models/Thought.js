2a092701bdbf41fa000d2321a980707038abae2a
valyastriz <valyastriz@gmail.com>
2024-09-07 18:18:16 -0400

    added reaction schema to thought.js model as a subdocument

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,34 @@
 const { Schema, model, Types } = require('mongoose');
 
+// schema to create Reaction subdocument
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (timestamp) => new Date(timestamp).toLocaleString(),
+        },
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+        id: false,
+    }
+);
 
 // schema to create Thought model
 
