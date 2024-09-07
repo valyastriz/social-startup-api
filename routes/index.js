77fd4f33fec063faa2138668dd3a8d73bb0f86be
valyastriz <valyastriz@gmail.com>
2024-09-07 19:10:36 -0400

    added logic to index.js in routes folder

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,9 @@
 // combines all API routes
+
+const router = require('express').Router();
+const apiRoutes = require('./api');
+
+// user the api routes
+router.use('/api', apiRoutes);
+
+module.exports = router;
