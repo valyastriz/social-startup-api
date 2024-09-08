513543ada82b5907044c89b978cacb0d06c64031
valyastriz <valyastriz@gmail.com>
2024-09-07 21:45:14 -0400

    removed deprecated code from connection.js file

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config(); 
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/socialnetwork', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/socialnetwork');
 
 module.exports = mongoose.connection;
