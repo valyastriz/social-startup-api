740b2723b9f79b273b63136d94b68a13c08b8881
valyastriz <valyastriz@gmail.com>
2024-09-07 21:13:00 -0400

    added deleteThought operation to thoughtController

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -63,3 +63,25 @@ const updateThought = async (req, res) => {
     }
 };
 
+// delete a thought by ID
+const deleteThought = async (req, res) => {
+    try {
+        const thought = await Thought.findOneAndDelete({ _id: req.params.thoughtId });
+        if (!thought) {
+            return res.status(404).json({ message: 'No thought with that ID '});
+        }
+
+        // remove thought from user's thought array
+        await User.findOneAndUpdate(
+            { thoughts: req.params.thoughtId },
+            { $pull: { thoughts: req.params.thoughtId } },
+            { new: true }
+        );
+
+        res.json({ message: 'Thought deleted!' });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+};
+
+module.exports = { getThoughts, getSingleThought, createThought, updateThought, deleteThought };
