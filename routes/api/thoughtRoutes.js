adc753c1833d599eaf186535b1fbba5c3f2cad66
valyastriz <valyastriz@gmail.com>
2024-09-07 21:19:23 -0400

    added routes to create and delete reactions to thoughtRoutes.js file

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -7,6 +7,18 @@ const {
     deleteThought,
 } = require('../../controllers/thoughtController');
 
+const {
+    addReaction,
+    removeReaction,
+} = require('../../controllers/thoughtController');
+
+// POST a reaction to a thought and DELETE a reaction
+router.route('/:thoughtId/reactions')
+  .post(addReaction);
+
+router.route('/:thoughtId/reactions/:reactionId')
+  .delete(removeReaction); 
+
 // GET all thought and POST a new thought
 router.route('/')
     .get(getThoughts)
