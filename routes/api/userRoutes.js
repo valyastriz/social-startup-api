3d2293da981849b66a452cb8587b7ee48f8d44e4
valyastriz <valyastriz@gmail.com>
2024-09-07 21:23:11 -0400

    added logic to userRoutes.js in api folder

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,1 +1,23 @@
 // combines user and thought routes
+
+const router = require('express').Router();
+const {
+    getUsers,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser,
+} = require('../../controllers/userController');
+
+// GET all users and POST a new user
+router.route('/')
+    .get(getUsers)
+    .post(createUser);
+
+// GET a single user, PUT to update a user and DELETE to remove a user
+router.route('/:userId')
+    .get(getSingleUser)
+    .put(updateUser)
+    .delete(deleteUser);
+
+module.exports = router;
